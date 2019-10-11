7eaafa2247280a0851c888476f570400fc49b1e8
Giorgos Konsoulakis <giorgoskonsoulakis@gmail.com>
2019-10-11 15:52:51 +0300

    to be deployed

---
+++ dist/precache-manifest.6ccdca22634269a7aca51a2bd8036e20.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "06ace57f05fbddfedd971620959575e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8661bec1068224acbdcdd9cb9b89649f",
+    "url": "/img/foyer.jpeg"
+  },
+  {
+    "revision": "95982ad0d6e0c849ab577d741059dae4",
+    "url": "/img/tooth.png"
+  },
+  {
+    "revision": "a1d89d85b15743e29f06",
+    "url": "/js/chunk-vendors.28b93824.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "445a48e12e1328e2d7ec5665d00f73df",
+    "url": "/img/vitsaro.png"
+  },
+  {
+    "revision": "acee4d9b4662f663b5fc6377deb30126",
+    "url": "/img/staprazis.png"
+  },
+  {
+    "revision": "714f9bf1987c0cc68ce357db0a72e148",
+    "url": "/img/oral_b.png"
+  },
+  {
+    "revision": "a9fa3fb0a4df5780d8461a58d223f4f4",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "e604a4139e0311b82675477e34e2d1bf",
+    "url": "/img/logo-el.png"
+  },
+  {
+    "revision": "714f9bf1987c0cc68ce357db0a72e148",
+    "url": "/img/oral_b.714f9bf1.png"
+  },
+  {
+    "revision": "445a48e12e1328e2d7ec5665d00f73df",
+    "url": "/img/vitsaro.445a48e1.png"
+  },
+  {
+    "revision": "fc235da91a150e2085ff",
+    "url": "/js/app.e3df279b.js"
+  },
+  {
+    "revision": "e604a4139e0311b82675477e34e2d1bf",
+    "url": "/img/logo-el.e604a413.png"
+  },
+  {
+    "revision": "a9fa3fb0a4df5780d8461a58d223f4f4",
+    "url": "/img/logo.a9fa3fb0.png"
+  },
+  {
+    "revision": "acee4d9b4662f663b5fc6377deb30126",
+    "url": "/img/staprazis.acee4d9b.png"
+  },
+  {
+    "revision": "9bc85242b67e5abaa8812c1d8dc06f52",
+    "url": "/img/39president.9bc85242.jpeg"
+  },
+  {
+    "revision": "27f30fc6e904b961d4f7ff532e5b9d97",
+    "url": "/img/colgate.27f30fc6.jpg"
+  },
+  {
+    "revision": "27f30fc6e904b961d4f7ff532e5b9d97",
+    "url": "/img/colgate.jpg"
+  },
+  {
+    "revision": "c011f9fbe91b3f32b791809cedb5a338",
+    "url": "/img/dental_co.png"
+  },
+  {
+    "revision": "5ae9dfe64d2009bb1ed535f0efa56c19",
+    "url": "/img/devliotis.jpg"
+  },
+  {
+    "revision": "c011f9fbe91b3f32b791809cedb5a338",
+    "url": "/img/dental_co.c011f9fb.png"
+  },
+  {
+    "revision": "5ae9dfe64d2009bb1ed535f0efa56c19",
+    "url": "/img/devliotis.5ae9dfe6.jpg"
+  },
+  {
+    "revision": "6a6f8a4212668921126ec3ebf9a118a4",
+    "url": "/img/FLOORPLAN_3.jpeg"
+  },
+  {
+    "revision": "6a6f8a4212668921126ec3ebf9a118a4",
+    "url": "/img/FLOORPLAN_3.6a6f8a42.jpeg"
+  },
+  {
+    "revision": "8661bec1068224acbdcdd9cb9b89649f",
+    "url": "/img/FLOORPLAN_2.jpeg"
+  },
+  {
+    "revision": "8661bec1068224acbdcdd9cb9b89649f",
+    "url": "/img/FLOORPLAN_2.8661bec1.jpeg"
+  },
+  {
+    "revision": "f6f1818e687b7e7e9f3b3064eccde41b",
+    "url": "/img/FLOORPLAN_1.jpg"
+  },
+  {
+    "revision": "f6f1818e687b7e7e9f3b3064eccde41b",
+    "url": "/img/FLOORPLAN_1.f6f1818e.jpg"
+  },
+  {
+    "revision": "9bc85242b67e5abaa8812c1d8dc06f52",
+    "url": "/img/39president.jpeg"
+  },
+  {
+    "revision": "a1d89d85b15743e29f06",
+    "url": "/css/chunk-vendors.d05054b5.css"
+  },
+  {
+    "revision": "fc235da91a150e2085ff",
+    "url": "/css/app.f7063a4b.css"
+  }
+];
--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,22 +1,33 @@
-import { register } from 'register-service-worker';
+/* eslint-disable no-console */
+import { register } from 'register-service-worker'
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
-    ready() {
-      console.log('App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB');
+    ready () {
+      // eslint-disable-next-line no-console
+      console.log(
+        'App is being served from cache by a service worker.\n' +
+        'For more details, visit https://goo.gl/AFskqB'
+      )
     },
-    cached() {
-      console.log('Content has been cached for offline use.');
+    registered () {
+      console.log('Service worker has been registered.')
     },
-    updated() {
-      console.log('New content is available; please refresh.');
+    cached () {
+      console.log('Content has been cached for offline use.')
     },
-    offline() {
-      console.log('No internet connection found. App is running in offline mode.');
+    updatefound () {
+      console.log('New content is downloading.')
     },
-    error(error) {
-      console.error('Error during service worker registration:', error);
+    updated () {
+      console.log('New content is available; please refresh.')
+      window.location.reload(true);
     },
-  });
+    offline () {
+      console.log('No internet connection found. App is running in offline mode.')
+    },
+    error (error) {
+      console.error('Error during service worker registration:', error)
+    }
+  })
 }
